067502a9199207d04777c58b12e3735537baa2c9
Anubhav Choudhary <ac.10edu@gmail.com>
2022-03-27 17:48:03 +0530

    Login route
    Signed-off-by: Anubhav Choudhary <ac.10edu@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # sih-backend
+
+## API workspace
+
+[https://app.getpostman.com/join-team?invite_code=f9a482745b86466c2f8c183198f8b4f0&target_code=d0844b36144f6ca7265fc27392b6aa3e](https://app.getpostman.com/join-team?invite_code=f9a482745b86466c2f8c183198f8b4f0&target_code=d0844b36144f6ca7265fc27392b6aa3e)
--- src/router.js
+++ src/router.js
@@ -5,4 +5,27 @@ router.get("/", (req, res) => {
   res.send("HELLO WORLD");
 });
 
+router.post("/login", (req, res) => {
+  console.log(req.body);
+  const aadhaarNumber = req.body.aadhaarNumber;
+
+  if (aadhaarNumber === 999999999999) {
+    // @TODO
+    res.json({
+      isOperator: false,
+    });
+    res.sendStatus(200);
+  } else if (aadhaarNumber === 888888888888) {
+    res.json({
+      isOperator: true,
+    });
+    res.sendStatus(200);
+  } else res.sendStatus(401);
+});
+
+// register complains
+// get complains
+// assign complain
+// complete complain
+
 module.exports = router;
